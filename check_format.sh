98f085bc34764b653f3e552ec20013ad18860406
Karina534 <smirnova7.04@mail.ru>
2023-12-09 18:12:27 +0300

    Test on txt

--- check_format.sh
+++ check_format.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-
-changed_files = $(git diff --cached --name-only --diff-filter=ACM)
-
-for file in $changed_files; do
-	if [[ ! $file =~ \.txt$ ]]; then
-		echo "Eror, $file is not txt"
-		exit 1
-	fi
-done
-
+#!/bin/bash 
+ 
+changed_files=$(git diff --cached --name-only --diff-filter=ACM) 
+ 
+for file in $changed_files; do 
+  if [[ ! $file =~ \.(txt|sh)$ ]]; then 
+    echo "Ошибка: Файл $file не имеет расширение .txt" 
+    exit 1 
+  fi 
+done 
+ 
 exit 0
